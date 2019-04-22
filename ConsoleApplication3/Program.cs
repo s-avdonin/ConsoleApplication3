597d9a378a95091f28a44fdc0a7b484a9f4bd673
s-avdonin <serhii.avdonin@gmail.com>
2019-04-22 23:14:17 +0300

    third commit

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication3
 {
 	class Program
 	{ 
-		// second commit 2
+		// Third commit 2
 		static readonly List<char> Alphabet = new List<char>() { 'а', 'б', 'в', 'г', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я' };
 
 		static void Main(string[] args)
